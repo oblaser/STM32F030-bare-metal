11a049a9b13862899391367038cd9ef711d2403c
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2024-12-27 20:29:26 +0100

    VS Code task settings

--- src/main.c
+++ src/main.c
@@ -15,8 +15,13 @@ int main()
 
     while (1)
     {
+#if 0
         if (GPIO_readPin(GPIO_BTN0)) { GPIO_setPin(GPIO_LED0); }
         else { GPIO_clrPin(GPIO_LED0); }
+#else
+        if (GPIO_readPin(GPIO_BTN0)) { GPIO_clrPin(GPIO_LED0); }
+        else { GPIO_setPin(GPIO_LED0); }
+#endif
     }
 
     return (-1);
