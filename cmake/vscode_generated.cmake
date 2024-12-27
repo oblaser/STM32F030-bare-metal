0f10a2e240055c21adbea0ed2c0fbe52afa1c3ca
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2024-12-27 17:03:54 +0100

    adapted project dir structure

--- cmake/vscode_generated.cmake
+++ cmake/vscode_generated.cmake
@@ -20,17 +20,10 @@ set(linker_script_SRC ${linker_script_SRC}
 
 # Sources
 set(sources_SRCS ${sources_SRCS}
-    
-	${CMAKE_CURRENT_SOURCE_DIR}/Src/main.c
-	${CMAKE_CURRENT_SOURCE_DIR}/Src/syscall.c
-	${CMAKE_CURRENT_SOURCE_DIR}/Src/sysmem.c
-	${CMAKE_CURRENT_SOURCE_DIR}/Startup/startup_stm32f030k6tx.s
 )
 
 # Include directories
 set(include_c_DIRS ${include_c_DIRS}
-    
-	${CMAKE_CURRENT_SOURCE_DIR}/Inc
 )
 set(include_cxx_DIRS ${include_cxx_DIRS}
     
